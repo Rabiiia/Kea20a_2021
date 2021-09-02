154faa9226da27b9a776e57957676427ee74f342
Rabia --- <acmverden@hotmail.com>
2021-09-02 10:57:15 +0200

    added changes

--- 4_Streams/RandomStream.java
+++ 4_Streams/RandomStream.java
@@ -5,7 +5,7 @@ public class RandomStream {
     public static void main(String[] args){
 
         Random random = new Random();
-        // System.out.println(random.nextInt()); // not a stream
+        // System.out.println(random.nextInt()); // not a streamm
         random.ints().limit(4).forEach((number) ->System.out.println(number));
     }
 }
